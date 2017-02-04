0a0cb12f7e04ba3f3842e227bda2bf6f47afcb35
Janne Sauvala <janne.sauvala@gmail.com>
2017-02-04 14:41:24 +0200

    Added basic artifacts for the solver.

---
+++ PuzzleSolver/src/Solver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PuzzleSolver.src
+{
+    public class Solver
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Solver is running");
+        }
+    }
+}
---
+++ PuzzleSolver/src/Tile.cs
@@ -0,0 +1,6 @@
+﻿namespace PuzzleSolver.src
+{
+    public class Tile
+    {
+    }
+}
