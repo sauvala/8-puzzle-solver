d2440f49853f3a8eca2dc270fa17618e33b0eab3
Janne Sauvala <janne.sauvala@gmail.com>
2017-02-04 14:49:19 +0200

    Board initialization.

--- PuzzleSolver/src/Board.cs
+++ PuzzleSolver/src/Board.cs
@@ -1,6 +1,15 @@
-﻿namespace PuzzleSolver.src
+﻿using System.Dynamic;
+
+namespace PuzzleSolver.src
 {
     public class Board
     {
+        public Board(int size)
+        {
+            Size = size;
+            Tiles = new Tile[size];
+        }
+        public int Size { get; }
+        public Tile[] Tiles { get; }
     }
 }
